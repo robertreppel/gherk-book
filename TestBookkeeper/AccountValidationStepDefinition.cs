ef5bc6dc25ccc98c1057870676c2a7ff9ee21647
Robert Laptop <rreppel@gmail.com>
2012-04-30 06:40:08 -0700

    account statement feature file table parsed.

--- TestBookkeeper/AccountValidationStepDefinition.cs
+++ TestBookkeeper/AccountValidationStepDefinition.cs
@@ -1,14 +1,37 @@
-﻿using TechTalk.SpecFlow;
+﻿using System;
+using System.Collections.Generic;
+using Bookkeeper.Infrastructure.Interfaces;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
 
 namespace TestBookkeeper
 {
     [Binding]
     public class AccountValidationStepDefinition
     {
-        [Then(@"the account ""Sales \(Services\)"" contains the following:")]
-        public void ThenTheAccountSalesServicesContainsTheFollowing(Table table)
+        [Then(@"the ""(.*)"" (\d+) account contains the following:")]
+        public void ThenTheAccountSalesServicesContainsTheFollowing(string accountName, int accountNumber, Table table)
         {
-            ScenarioContext.Current.Pending();
+            var expectedAccountTransactions = AccountTransform(table);
+
+            //var account = (IAccount)ScenarioContext.Current[account];
+            Assert.Inconclusive("Not implemented.");
+        }
+
+        [StepArgumentTransformation]
+        public IEnumerable<ITransaction> AccountTransform(Table account)
+        {
+            var testTrialBalanceLineItems = account.CreateSet<Transaction>();
+            return testTrialBalanceLineItems;
+        }
+
+        public class Transaction : ITransaction {
+            public DateTime TransactionDate { get; private set; }
+            public int AccountNo { get; private set; }
+            public string TransactionReference { get; private set; }
+            public decimal Debit { get; private set; }
+            public decimal Credit { get; private set; }
         }
     }
 }
